cbdfa3e9c8aab56efe6158d1ce83548577ec7a0d
qoo23502004 <qoo23502004@yahoo.com.tw>
2019-03-14 01:05:15 +0800

    something change

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def handle_memberJoined(event):
     #tmpToken = event.reply_token
     #newMember = line_bot_api.get_profile(event.source.user_id)
     #profile = line_bot_api.get_profile(event.source.user_id)
-    message=TextMessage(text="歡迎新觀眾的加入^^喵嗚，請記得去記事本簽到唷><")
+    message=TextMessage(text="歡迎加入大同寶寶麻將切磋群，進來後請詳細閱讀記事本，任何事務與公告都以記事本為主唷^^")
     line_bot_api.reply_message(event.reply_token, message)
 
 #@handler.add(MemberLeaveEvent)
@@ -66,52 +66,8 @@ def handle_memberJoined(event):
 
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    cityDict={"!嘉義縣":0,"!新北市":1,"!嘉義市":2,"!新竹縣":3,"!新竹市":4,"!台北市":5,"!台南市":6,"!宜蘭縣":7,"!苗栗縣":8,"!雲林縣":9,"!花蓮縣":10,"!台中市":11,"!台東縣":12,"!桃園市":13,"!南投縣":14,"!高雄市":15,"!金門縣":16,"!屏東縣":17,"!基隆市":18,"!澎湖縣":19,"!彰化縣":20,"!連江縣":21}
-    feed=["!早餐","!午餐","!下午茶","!晚餐","!宵夜","!消夜"]
-    adminID=["U5bd55d60b2112ffb591908d043b7267b","Ua949af5635ed28ba3abbd377b0f276b1","Ue01de340eb97da851243467b6ba179f2",
-             "U600f407b05672dd82a6fb54ec5f18270","Ue485a9bf0f3761e976869456dc0424c9","Ucf6c24c63299afaafb8f123db1e02d08",
-             "Uf5b11c276ff1bcec02ae5162afd03a7c"]
-    ytKeyword=""
-    pushAns=""
-    cmdContent=""
-    glKeyword=""
-	
-    if event.message.text=="咬咬我愛你" or event.message.text=="咬咬我愛妳" :
-        profile = line_bot_api.get_group_member_profile(event.source.group_id,event.source.user_id)
-        message = TextSendMessage(text="我也愛你唷 "+profile.display_name)
-        line_bot_api.reply_message(event.reply_token, message)
-
-    if event.message.text=="!pic":
-        #profile = line_bot_api.get_profile(event.source.user_id)
-        message = ImageSendMessage(
-        original_content_url='https://i.imgur.com/NhqT8yT.jpg',
-        preview_image_url='https://i.imgur.com/NhqT8yT.jpg'
-        )
-        #textMessage = TextSendMessage(text="testMessage")
-        #message = TextSendMessage(text="◎序號兌換至myVideo官網/APP「兌換儲值」輸入序號 「vv0z1」兌換使用\nhttps://reurl.cc/XjRx3")
-        line_bot_api.reply_message(event.reply_token, message)
-        #line_bot_api.reply_message(event.reply_token, textMessage)
-
-    if event.message.text=="!表單":
-        message = TextSendMessage(text="請大家幫忙填一下VV的週邊意願調查\nhttps://goo.gl/vwxMG3")
-        line_bot_api.reply_message(event.reply_token, message)
-    
-    if event.message.text=="!狀態":
-        string = checkState()
-        message = TextSendMessage(text=string)
-        line_bot_api.reply_message(event.reply_token, message)
-
-    if event.message.text in cityDict:
-        profile = line_bot_api.get_group_member_profile(event.source.group_id,event.source.user_id)       
-        string = weatherSearch(event.message.text)
-        message = TextSendMessage(text=profile.display_name+" 所查詢的天氣資料如下：\n"+string)
-        line_bot_api.reply_message(event.reply_token, message)
-
-    if event.message.text in feed:
-        string = food(event.message.text)
-        message = TextSendMessage(text="真心推薦: "+string)
-        line_bot_api.reply_message(event.reply_token, message)
+def handle_message(event):   
+    #adminID=[""]
 
     if event.message.text=="!GID":           
         message = TextSendMessage(text=event.source.group_id)
@@ -129,38 +85,6 @@ def handle_message(event):
         message = TextSendMessage(text="測試權限成功")
         line_bot_api.reply_message(event.reply_token, message)
 
-    
-
-    ans=event.message.text
-    ansCut=ans.split(' ')
-    if ansCut[0]=="!機器人" and len(ansCut)>=2 and event.source.user_id in adminID:
-        for i in range(1,len(ansCut)):
-            pushAns=pushAns+ansCut[i]+" "    
-        line_bot_api.push_message("Cffc4e3c256a638f9f11e89c1171a9f4b", TextSendMessage(text=pushAns))
-        
-         
-    
-    keyword=event.message.text
-    keywordCut=keyword.split(' ')
-    if keywordCut[0]=="!sr" or keywordCut[0]=="!SR" and len(keywordCut)>=2:
-        for i in range(1,len(keywordCut)):	
-            ytKeyword=ytKeyword+keywordCut[i]+" "
-        content = musicSearch(ytKeyword)
-        profile = line_bot_api.get_group_member_profile(event.source.group_id,event.source.user_id)
-        message = TextSendMessage(text=profile.display_name+" 查詢的影片在這 https://www.youtube.com/watch?v="+content)
-        
-        line_bot_api.push_message("C4fe2e6fd176c7822ed60a78d3941aaea", TextSendMessage(text=event.source.user_id+" "+profile.display_name+" 查詢的影片在這 https://www.youtube.com/watch?v="+content))
-        line_bot_api.reply_message(event.reply_token, message)
-
-
-    googleKeyword=event.message.text
-    googleKeywordCut=googleKeyword.split(' ')
-    if googleKeywordCut[0]=="!google" and len(googleKeywordCut)>=2:
-        for i in range(1,len(googleKeywordCut)):
-            glKeyword=glKeyword+googleKeywordCut[i]+" "
-        content = googleSearch(glKeyword)
-        message = TextSendMessage(text=content)
-        line_bot_api.reply_message(event.reply_token, message)
     
 
 
